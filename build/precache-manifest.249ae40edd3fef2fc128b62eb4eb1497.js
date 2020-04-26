c2f4acbb514f088073f488d55fe61996abacbe13
Juan Carlos Cancela <bak@Juans-MacBook-Pro-3.local>
2020-04-26 19:54:06 -0300

    arreglado problema

---
+++ build/precache-manifest.249ae40edd3fef2fc128b62eb4eb1497.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "506df6c9da3c2cdf9ab0598fe7d9885c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1ec55cf59087ce9e6f7",
+    "url": "/static/css/2.74189dbb.chunk.css"
+  },
+  {
+    "revision": "31fdf545fe6ac0edc5c9",
+    "url": "/static/css/main.aec39ff3.chunk.css"
+  },
+  {
+    "revision": "b1ec55cf59087ce9e6f7",
+    "url": "/static/js/2.e996a555.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.e996a555.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "31fdf545fe6ac0edc5c9",
+    "url": "/static/js/main.a7b153ca.chunk.js"
+  },
+  {
+    "revision": "556e2a4f58cffaeef446",
+    "url": "/static/js/runtime-main.7c02405e.js"
+  },
+  {
+    "revision": "41c4237912e1d725ec7e137481436d7f",
+    "url": "/static/media/madera.41c42379.png"
+  },
+  {
+    "revision": "11ceffbda8998acd0dc505916e08cb7f",
+    "url": "/static/media/piedra.11ceffbd.png"
+  }
+]);
